f638c8acdbca0af32979f720518efb099a3886ec
shivam-arora-13 <shivamarora.bt19cse@pec.edu.in>
2023-04-04 14:29:18 +0530

    basic UI template created

---
+++ index.js
@@ -0,0 +1,34 @@
+const ejs = require('ejs');
+const express = require('express');
+const path = require("path")
+
+
+const app = express();
+
+
+// app.use(express.static('public'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(
+    "/css",
+    express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+  )
+  app.use(
+    "/js",
+    express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+  )
+  app.use("/js", express.static(path.join(__dirname, "node_modules/jquery/dist")))
+  
+
+app.get('/', (req, res) => {
+    const data = {
+        key : "2237769"
+    };
+    res.render('template.ejs', data);
+});
+
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
